b6390d782ab4144610191a529b4d1904f41245be
Akila Induranga <akila.99g@gmail.com>
2025-05-27 15:06:18 +0530

    Update main.go

--- hello-world-go-task/main.go
+++ hello-world-go-task/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	for i, arg := range os.Args[1:] {
 		fmt.Println("Arg", i, ": ", arg)
-	}
-
-	// Print time, sleep 60 seconds, repeat 30 times
-	for i := 0; i < 30; i++ {
-		fmt.Printf("Time %d: %s\n", i+1, time.Now().Format("2006-01-02 15:04:05"))
-		time.Sleep(60 * time.Second)
+		// Print time, sleep 60 seconds, repeat 30 times
+		for i := 0; i < 30; i++ {
+			fmt.Printf("Time %d: %s\n", i+1, time.Now().Format("2006-01-02 15:04:05"))
+			time.Sleep(60 * time.Second)
+		}
 	}
 }
